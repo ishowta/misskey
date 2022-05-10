9ae4a03cc11d03a5d2221e8105807c569eeeec33
ishowta <ishowta@gmail.com>
2022-05-10 13:07:48 +0900

    fix: Fix transaction error on update-meta

--- packages/backend/src/server/api/endpoints/admin/update-meta.ts
+++ packages/backend/src/server/api/endpoints/admin/update-meta.ts
@@ -397,6 +397,8 @@ export default define(meta, paramDef, async (ps, me) => {
 	}
 
 	await db.transaction(async transactionalEntityManager => {
+		await transactionalEntityManager.getRepository(Meta).createQueryBuilder('meta').setLock('pessimistic_read').execute();
+
 		const metas = await transactionalEntityManager.find(Meta, {
 			order: {
 				id: 'DESC',
